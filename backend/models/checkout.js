68917719ee99856719289336d9761b74248d829d
asjadahamed77 <asjadahamed77@gmail.com>
2025-03-05 13:46:22 +0530

    created checkout model

---
+++ backend/models/checkout.js
@@ -0,0 +1,91 @@
+import mongoose from "mongoose";
+
+const checkoutItemSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "productModel",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+const checkoutSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "userModel",
+  },
+  checkoutItems: [checkoutItemSchema],
+  shippingAddress: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+
+  },
+    isPaid: {
+        type: Boolean,
+        required: true,
+      
+    },
+    paidAt: {
+        type: Date,
+    },
+    parymentStatus: {
+        type: String,
+        default: "pending"
+    },
+    paymentDetails:{
+        type: mongoose.Schema.Types.Mixed
+    },
+    isFinalized: {
+        type: Boolean,
+        default: false
+    },
+    finalizedAt: {
+        type: Date
+    },
+
+
+
+},
+{ timestamps: true }
+);
+
+
+const checkoutModel = mongoose.models.checkout || mongoose.model("checkout", checkoutSchema);
+
+export default checkoutModel;
