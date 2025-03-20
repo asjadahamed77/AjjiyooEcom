552d466d2fd204f498468a7f321cc84f8d324a4c
asjadahamed77 <asjadahamed77@gmail.com>
2025-03-20 22:03:13 +0530

    created adminSlice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,13 +4,15 @@ import productReducer from './slices/productsSlice'
 import cartReducer from './slices/cartSlice'
 import checkourReducer from './slices/checkoutSlice'
 import orderReducer from './slices/orderSlice'
+import adminReducer from './slices/adminSlice'
 const store = configureStore({
     reducer: {
        auth: authReducer,
        products: productReducer,
        cart: cartReducer,
        checkout: checkourReducer,
-       orders: orderReducer
+       orders: orderReducer,
+       admin: adminReducer
     }
 })
 
