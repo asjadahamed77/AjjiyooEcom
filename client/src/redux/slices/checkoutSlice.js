35f270da2cf452459352de65a7ec4db21e75630d
asjadahamed77 <asjadahamed77@gmail.com>
2025-03-20 20:04:03 +0530

    completed checkout slice

---
+++ client/src/redux/slices/checkoutSlice.js
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Asynk thunk to create a checkout session
+export const createCheckout = createAsyncThunk(
+  "checkout/createCheckout",
+  async (checkoutData, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/checkout`,
+        checkoutData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      console.error(error);
+      return rejectWithValue(error.data);
+    }
+  }
+);
+
+const checkoutSlice = createSlice({
+  name: "checkout",
+  initialState: {
+    checkout: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createCheckout.pending, (state) => {
+        (state.loading = true), (state.error = null);
+      })
+      .addCase(createCheckout.fulfilled, (state, action) => {
+        (state.loading = false), (state.error = action.payload);
+      })
+      .addCase(createCheckout.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      });
+  },
+});
+
+export default checkoutSlice.reducers
