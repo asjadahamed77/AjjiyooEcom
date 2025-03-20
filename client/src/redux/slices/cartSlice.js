99efd9586f42ec2c5549142cc1cf67761a828d2e
asjadahamed77 <asjadahamed77@gmail.com>
2025-03-20 19:40:32 +0530

    completed cartSlice

--- client/src/redux/slices/cartSlice.js
+++ client/src/redux/slices/cartSlice.js
@@ -142,45 +142,75 @@ const cartSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-    .addCase(fetchCart.pending, (state) => {
-      state.loading = true;
-      state.error = null;
-    })
-    .addCase(fetchCart.fulfilled, (state, action) => {
-      state.loading = false;
-      state.cart = action.payload;
-      saveCartToStorage(action.payload)
-    })
-    .addCase(fetchCart.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.error.message || "Failed to fetch cart."
-    })
-    .addCase(addToCart.pending, (state) => {
-      state.loading = true;
-      state.error = null;
-    })
-    .addCase(addToCart.fulfilled, (state, action) => {
-      state.loading = false;
-      state.cart = action.payload;
-      saveCartToStorage(action.payload)
-    })
-    .addCase(addToCart.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.payload?.message || "Failed to add to cart."
-    })
-    .addCase(updateCartItemQuantity.pending, (state) => {
-      state.loading = true;
-      state.error = null;
-    })
-    .addCase(updateCartItemQuantity.fulfilled, (state, action) => {
-      state.loading = false;
-      state.cart = action.payload;
-      saveCartToStorage(action.payload)
-    })
-    .addCase(updateCartItemQuantity.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.payload?.message || "Failed to add to cart."
-    })
-    
+      .addCase(fetchCart.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchCart.fulfilled, (state, action) => {
+        state.loading = false;
+        state.cart = action.payload;
+        saveCartToStorage(action.payload);
+      })
+      .addCase(fetchCart.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to fetch cart.";
+      })
+      .addCase(addToCart.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addToCart.fulfilled, (state, action) => {
+        state.loading = false;
+        state.cart = action.payload;
+        saveCartToStorage(action.payload);
+      })
+      .addCase(addToCart.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Failed to add to cart.";
+      })
+      .addCase(updateCartItemQuantity.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateCartItemQuantity.fulfilled, (state, action) => {
+        state.loading = false;
+        state.cart = action.payload;
+        saveCartToStorage(action.payload);
+      })
+      .addCase(updateCartItemQuantity.rejected, (state, action) => {
+        state.loading = false;
+        state.error =
+          action.payload?.message || "Failed to update cart quantity.";
+      })
+      .addCase(removeFromCart.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(removeFromCart.fulfilled, (state, action) => {
+        state.loading = false;
+        state.cart = action.payload;
+        saveCartToStorage(action.payload);
+      })
+      .addCase(removeFromCart.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Failed to remove item.";
+      })
+      .addCase(mergeCart.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(mergeCart.fulfilled, (state, action) => {
+        state.loading = false;
+        state.cart = action.payload;
+        saveCartToStorage(action.payload);
+      })
+      .addCase(mergeCart.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Failed to merge cart.";
+      });
   },
 });
+
+export const { clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
